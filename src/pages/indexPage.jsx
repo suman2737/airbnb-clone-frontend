05fda0d656f65c09db28397a764ba163f3a29e32
suman2737 <yadavsuman27031999@gmail.com>
2025-06-02 23:17:48 +0530

    airbnb-clone

--- src/pages/indexPage.jsx
+++ src/pages/indexPage.jsx
@@ -16,7 +16,7 @@ export default function IndexPage() {
         <Link to={'/place/'+place._id}>
           <div className="flex mb-2 bg-gray-500 rounded-2xl">
             {place.photos?.[0] && (
-              <img className="object-cover rounded-2xl aspect-square" src={'http://localhost:4000/uploads/' + place.photos?.[0]} alt=""/>
+              <img className="object-cover rounded-2xl aspect-square" src={'/https://airbnb-clone-backend-beryl.vercel.app/uploads/' + place.photos?.[0]} alt=""/>
             )}
           </div>
           <h2 className="font-bold">{place.address}</h2>
